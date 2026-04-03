693b901ed07141bb1d70cae3e1b183d9ea56185c
Ivan Drozdov <ivan18drozdov@gmail.com>
2026-04-03 15:34:42 +0300

    refactor(ad-edit-page): narrow section contracts

--- src/pages/ad-edit-page/model/index.ts
+++ src/pages/ad-edit-page/model/index.ts
@@ -1,5 +1,10 @@
 export { useAdEditPageModel } from "./use-ad-edit-page-model"
 export type {
+  AdEditAiToolsSectionProps,
+  AdEditCategoryChangeDialogProps,
+  AdEditDraftRestoreDialogProps,
+  AdEditFooterActionsProps,
+  AdEditFormSectionProps,
   AdEditPageModel,
   AdEditPageReadyState
 } from "./ad-edit-page.contract"
